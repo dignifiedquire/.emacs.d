c62f2a0009126771176e2ea29c8c7cce99fd872b
dignifiedquire <dignifiedquire@gmail.com>
2014-03-13 21:30:24 +0100

    Store desktop on exit.

--- init.el
+++ init.el
@@ -14,6 +14,15 @@
       emacs-tmp-dir)
 
 
+;; Save desktop
+(desktop-save-mode 1)
+(setq history-length 250)
+(add-to-list 'desktop-globals-to-save 'file-name-history)
+(add-to-list 'desktop-modes-not-to-save 'dired-mode)
+(add-to-list 'desktop-modes-not-to-save 'Info-mode)
+(add-to-list 'desktop-modes-not-to-save 'info-lookup-mode)
+(add-to-list 'desktop-modes-not-to-save 'fundamental-mode)
+
 ;; Styling
 (load-theme 'zenburn t)
 
