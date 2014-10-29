794e66abc7518d3a7e76f9b62f9abae7c2d40f81
dignifiedquire <dignifiedquire@gmail.com>
2014-10-29 10:08:13 +0100

    Fix color settings.

--- init.el
+++ init.el
@@ -1,6 +1,5 @@
 ;; Load Dependencies
 (require 'cask "/usr/local/Cellar/cask/0.7.2/cask.el")
-;;(require 'cask "~/.cask/cask")
 (cask-initialize)
 
 (add-to-list 'load-path "~/.emacs.d/vendor")
@@ -47,18 +46,7 @@
 
 
 ;; CoffeeScript
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
- '(coffee-tab-width 2)
- '(custom-safe-themes (quote ("146d24de1bb61ddfa64062c29b5ff57065552a7c4019bee5d869e938782dfc2a" default)))
- '(fci-rule-color "#383838")
- '(vc-annotate-background "#2B2B2B")
- '(vc-annotate-color-map (quote ((20 . "#BC8383") (40 . "#CC9393") (60 . "#DFAF8F") (80 . "#D0BF8F") (100 . "#E0CF9F") (120 . "#F0DFAF") (140 . "#5F7F5F") (160 . "#7F9F7F") (180 . "#8FB28F") (200 . "#9FC59F") (220 . "#AFD8AF") (240 . "#BFEBBF") (260 . "#93E0E3") (280 . "#6CA0A3") (300 . "#7CB8BB") (320 . "#8CD0D3") (340 . "#94BFF3") (360 . "#DC8CC3"))))
- '(vc-annotate-very-old-color "#DC8CC3"))
+'(coffee-tab-width 2)
 
 ;; Organizational
 
