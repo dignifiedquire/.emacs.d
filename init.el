2264cec7a760be596337f867ec99940b7b7b11ac
dignifiedquire <dignifiedquire@gmail.com>
2015-02-26 13:27:47 +0100

    color preview

--- init.el
+++ init.el
@@ -217,3 +217,19 @@
 
 ;; Disable scss mode
 (setq scss-compile-at-save nil)
+
+;; Hex color preview
+(defun xah-syntax-color-hex ()
+  "Syntax color hex color spec such as #ff1100 in current buffer."
+  (interactive)
+  (font-lock-add-keywords
+   nil
+   '(("#[abcdef[:digit:]]\\{6\\}"
+      (0 (put-text-property
+          (match-beginning 0)
+          (match-end 0)
+          'face (list :background (match-string-no-properties 0)))))))
+  (font-lock-fontify-buffer)
+  )
+(add-hook 'css-mode-hook 'xah-syntax-color-hex)
+(add-hook 'html-mode-hook 'xah-syntax-color-hex)
