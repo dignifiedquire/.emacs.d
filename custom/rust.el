0db94e17ace64ff68b3e8c61686b033bdcb0b640
dignifiedquire <dignifiedquire@gmail.com>
2018-07-04 11:18:45 +0200

    improve theme and rust modes

--- custom/rust.el
+++ custom/rust.el
@@ -5,7 +5,7 @@
 (setq racer-rust-src-path "~/.rustup/toolchains/nightly-x86_64-apple-darwin/lib/rustlib/src/rust/src") ;; Rust source code PATH
 
 (setq rust-format-on-save t)
-;; (add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'rust-mode-hook #'racer-mode)
 ;; (add-hook 'racer-mode-hook #'eldoc-mode)
 ;; (add-hook 'racer-mode-hook #'company-mode)
 
